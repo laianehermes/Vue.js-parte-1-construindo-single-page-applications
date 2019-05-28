a74dea44c5783b5700e3e37fdc05922aa34d9aaf
laianehermes <laiane.sp@hotmail.com>
2019-05-28 01:07:31 -0300

    Conteudo inicial do curso

---
+++ alurapic/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <img v-for="foto in fotos" :src="foto.url" :alt="foto.titulo">
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      titulo: "Alurapic",
+      fotos: []
+
+    }
+  },
+  created() {
+
+    alert('Funcionou!');
+  }
+}
+</script>
+<style>
+</style>
